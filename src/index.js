dd22cff21f9f5e9e6b3bd71d1765995c5b6bea55
OdeliaFink <odelia.finkel@gmail.com>
2023-07-21 22:14:15 -0400

    refactor

--- src/index.js
+++ src/index.js
@@ -1,27 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Card from 'react-bootstrap/Card';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import App from './App';
 
-import reducer from './reducer';
-
-import LoginForm from './LoginForm';
-
-const store = createStore(reducer);
-
-function App() {
-  return (
-    <Provider store={store}>
-      <div className="App">
-        <Card body>
-          <LoginForm />
-        </Card>
-      </div>
-    </Provider>
-  );
-}
-
-const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
